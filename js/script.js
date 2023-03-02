8c96aa15bfed9db936dab4a6ad9f14c9535fd1b4
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-02 13:23:01 +0100

    feat: inizializazione esercizio + pseudocodice

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+Consigli del giorno
+1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+*/
+
+/*
+PSEUDOCODICE
+
+-chiedo all'utente di inserire una parola
+- SE le lettere a partire dalla prima fino alla lettera di mezzo della parola sono uguali alle lettere a partire dall'ultima fino alla lettera di mezzo
+  ° la parola è palindroma
+  ALTRIMENTI
+  ° la parola non è palindroma
+
+  - chiedo all'utente se sceglie pari o dispari
+  - chiedo all'utente di inserire un numero da 1 a 5
+  - genero un numero random da 1 a 5 per il computer
+  - sommo il numero dell'utente al numero del computer
+  - stabilisco se la somma è pari o dispari
+  - SE l'utente ha scelto pari e la somma è pari
+    ° l'utente vince
+    ALTRIMENTI
+    ° l'utente perde
+*/
